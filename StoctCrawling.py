79868844228fd85e43167f3232eec15968f81373
gunlee <gunlee1226>
2021-03-12 11:12:34 +0900

    add

---
+++ StoctCrawling.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
+
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'lxml')
+
+stock_head = soup.find("thead").findAll("th")
+data_head = [head.get_text() for head in stock_head]
+
+stock_list = soup.find("table", attrs={"class" : "type_2"}).find("tbody").findAll("tr")
+
+for stock in stock_list:
+    if len(stock) > 1:
+        print(stock.get_text().split())
